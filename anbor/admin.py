d6f7a10a44908bc639cbba4029dc159874da11b1
Azizjon <azizaliev2337@gmail.com>
2024-01-31 14:18:09 +0500

    use product

--- anbor/admin.py
+++ anbor/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 
 class BaseModelAdmin(admin.ModelAdmin):
@@ -11,3 +11,10 @@ class CategoryAdmin(BaseModelAdmin):
     list_display = ('name', 'created_at', 'updated_at',)
     search_fields = ('name',)
 
+
+@admin.register(Product)
+class ProductAdmin(BaseModelAdmin):
+    list_display = ('title', 'category', 'created_at',)
+    autocomplete_fields = ('category',)
+    search_fields = ('title', 'category__name',)
+    list_filter = ('category',)
--- anbor/models.py
+++ anbor/models.py
@@ -5,10 +5,26 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        
+
     name = models.CharField(verbose_name='Название', max_length=200, unique=True, db_index=True)
     created_at = models.DateTimeField(verbose_name='Время создание', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Время обновление', auto_now=True)
-    
+
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    image = models.ImageField(verbose_name='Изоброжение', upload_to='products/', blank=True, null=True)
+    title = models.CharField(verbose_name='Название', max_length=200)
+    description = models.TextField(verbose_name='Описание', blank=True)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='Время создание', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Время обновление', auto_now=True)
+
+    def __str__(self):
+        return self.title
